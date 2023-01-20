fc57c300b091185cc85af17cc7ba40fe3cac3ad9
Pablo del Barrio <pablodbb@gmail.com>
2023-01-20 21:53:46 +0100

    Back to fetch questions from opentrivia

--- game.js
+++ game.js
@@ -13,14 +13,14 @@ let questionCounter = 0;
 let availableQuestions = [];
 let questions = [];
 // "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
-// fetch("https://opentdb.com/api.php?amount=10&category=11&type=multiple")
-fetch("http://localhost:3333/questions")
+fetch("https://opentdb.com/api.php?amount=10&category=11&type=multiple")
+  // fetch("http://localhost:3333/questions")
   .then((res) => {
     return res.json();
   })
   .then((loadedQuestions) => {
     console.log(loadedQuestions);
-    questions = loadedQuestions.map((loadedQuestion) => {
+    questions = loadedQuestions.results.map((loadedQuestion) => {
       const formattedQuestion = {
         question: loadedQuestion.question,
       };
